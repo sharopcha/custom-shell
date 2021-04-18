c21a8cabdbf4d683532c1ad688022d5d2563aa4a
sharopcha <napster1202@gmail.com>
2021-04-19 02:47:26 +0300

    adding some functions and fixing existing

--- igps_shell.c
+++ igps_shell.c
@@ -1,11 +1,15 @@
+#include <time.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <sys/wait.h>
 #include <readline/readline.h>
 #include <readline/history.h>
+#include <dirent.h>
 
 #define MAXCOM 1000 // max number of letters to be supported
 #define MAXLIST 100 // max number of commands to be supported
@@ -18,9 +22,9 @@ void init_shell()
 {
     clear();
     showLogo();
-    char *username = getenv("USER");
-    printf("\n\n\nUSER is: @%s", username);
-    printf("\n");
+    // char *username = getenv("USER");
+    // printf("\n\n\nUSER is: @%s", username);
+    // printf("\n");
 }
 
 // Function to take input
@@ -151,13 +155,31 @@ void openHelp()
          "\nList of Commands supported:"
          "\n>cd"
          "\n>ls"
+         "\n>copy"
+         "\n>cat"
+         "\n>move"
+         "\n>remove"
+         "\n>calculator"
+         "\n>permutate"
+         "\n>time"
+         "\n>piramide"
+         "\n>palindrome"
          "\n>exit"
+         "\n>clear\tclears current terminal view"
          "\n>all other general commands available in UNIX shell"
          "\n>pipe handling"
          "\n>improper space handling");
 
     return;
 }
+
+// Clear the terminal
+void clearTerminal()
+{
+    clear();
+}
+
+// Commands created by ourselves
 void calculator()
 {
     char operator;
@@ -186,19 +208,235 @@ void calculator()
         printf("Error! operator is not correct");
     }
 }
-void Useit()
+
+void swap(char *x, char *y)
 {
-    char *cmd = "echo";
-    char *argv[2];
-    argv[0] = "echo";
-    argv[1] = "hello";
-    execvp(cmd, argv);
+    char temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+void permute(char *a, int l, int r)
+{
+    int i;
+    if (l == r)
+        printf("%s\n", a);
+    else
+    {
+        for (i = l; i <= r; i++)
+        {
+            swap((a + l), (a + i));
+            permute(a, l + 1, r);
+            swap((a + l), (a + i)); // backtrack
+        }
+    }
+}
+
+void permutate()
+{
+    printf("Please enter a word to permutate it:");
+    char str[20];
+    scanf("%s", str);
+    int a = strlen(str);
+    permute(str, 0, a - 1);
+}
+
+void piramide()
+{
+    int i, j, spc, rows, k;
+    printf("Input number of rows : ");
+    scanf("%d", &rows);
+    spc = rows + 4 - 1;
+    for (i = 1; i <= rows; i++)
+    {
+        for (k = spc; k >= 1; k--)
+        {
+            printf(" ");
+        }
+
+        for (j = 1; j <= i; j++)
+            printf("* ");
+        printf("\n");
+        spc--;
+    }
+}
+
+void palindrome()
+{
+    int n, reversedN = 0, remainder, originalN;
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+    originalN = n;
+
+    // reversed integer is stored in reversedN
+    while (n != 0)
+    {
+        remainder = n % 10;
+        reversedN = reversedN * 10 + remainder;
+        n /= 10;
+    }
+
+    // palindrome if orignalN and reversedN are equal
+    if (originalN == reversedN)
+        printf("%d is a palindrome.", originalN);
+    else
+        printf("%d is not a palindrome.", originalN);
+}
+
+int showTime()
+{
+    time_t cur_time;
+    char *cur_t_string;
+    cur_time = time(NULL);
+    if (cur_time == ((time_t)-1))
+    {
+        (void)fprintf(stderr, "Failure to get the current time.\n");
+        return 1;
+    }
+    cur_t_string = ctime(&cur_time); //convert to local time format
+    if (cur_t_string == NULL)
+    {
+        (void)fprintf(stderr, "Failure to convert the current time.\n");
+        return 1;
+    }
+    (void)printf("\n The Current time is : %s \n", cur_t_string);
+    return 1;
+}
+
+// function to immitate cp command
+int copyFile(char *argv[])
+{
+    int src; //source file
+    int dst; //destination file
+    int n;   // tracking variable
+    char buf[1];
+
+    src = open(argv[1], O_RDONLY);
+    if (src == -1)
+    {
+        perror("can't find the source file");
+        return 1;
+    }
+
+    dst = creat(argv[2], 777);
+    dst = open(argv[2], O_WRONLY);
+    if (dst == -1)
+    {
+        perror("can't create or open destination file");
+        return 1;
+    }
+
+    while ((n = read(src, buf, 1)) > 0)
+    {
+        write(dst, buf, 1);
+    }
+    close(src);
+    close(dst);
+
+    return 1;
+}
+
+// function to immitate cat command
+int readWrite(char *argv[])
+{
+
+    int src; //source file
+    int n;   // tracking variable
+    char buf[1];
+
+    src = open(argv[1], O_RDONLY);
+    if (src == -1)
+    {
+        perror("can't find the source file");
+        return 1;
+    }
+
+    while ((n = read(src, buf, 1)) > 0)
+    {
+        write(1, buf, 1);
+    }
+    close(src);
+    return 1;
+}
+
+// function to immitate list command
+int list(void)
+{
+
+    struct dirent *contents;
+    DIR *folder = opendir(".");
+
+    if (folder == NULL)
+    {
+        printf("cannot open the folder");
+        return 1;
+    }
+    while ((contents = readdir(folder)) != NULL)
+    {
+        printf("%s\n", contents->d_name);
+    }
+
+    closedir(folder);
+    return 1;
+}
+
+// function to immitate mv command
+int move(char *argv[])
+{
+    int src; //source file
+    int dst; //destination file
+    int n;   // tracking variable
+    char buf[1];
+
+    src = open(argv[1], O_RDONLY);
+    if (src == -1)
+    {
+        perror("can't find the source file");
+        return 1;
+    }
+
+    dst = creat(argv[2], 0777);
+    dst = open(argv[2], O_WRONLY);
+    if (dst == -1)
+    {
+        perror("can't create or open destination file");
+        return 1;
+    }
+
+    while ((n = read(src, buf, 1)) > 0)
+    {
+        write(dst, buf, 1);
+    }
+    close(src);
+    remove(argv[1]);
+    close(dst);
+    return 1;
+}
+
+// function to immitate rm command
+int removeFile(char *argv[])
+{
+    if (argv[1] == "--help")
+    {
+        printf("\nusage: rm FileTodelete\n");
+    }
+    int status;
+    status = remove(argv[1]);
+    if (status == 0)
+    {
+        printf("successfull removed\n");
+    }
+    else
+    {
+        printf("The file is not removed\n");
+    }
 }
 
 // Function to execute builtin commands
 int ownCmdHandler(char **parsed)
 {
-    int NoOfOwnCmds = 5, i, switchOwnArg = 0;
+    int NoOfOwnCmds = 15, i, switchOwnArg = 0;
     char *ListOfOwnCmds[NoOfOwnCmds];
     char *username;
 
@@ -207,7 +445,16 @@ int ownCmdHandler(char **parsed)
     ListOfOwnCmds[2] = "help";
     ListOfOwnCmds[3] = "hello";
     ListOfOwnCmds[4] = "calculator";
-    ListOfOwnCmds[5] = "surprise";
+    ListOfOwnCmds[5] = "permutate";
+    ListOfOwnCmds[6] = "showTime";
+    ListOfOwnCmds[7] = "piramide";
+    ListOfOwnCmds[8] = "palindrome";
+    ListOfOwnCmds[9] = "cat";
+    ListOfOwnCmds[10] = "ls";
+    ListOfOwnCmds[11] = "copy";
+    ListOfOwnCmds[12] = "move";
+    ListOfOwnCmds[13] = "clear";
+    ListOfOwnCmds[14] = "remove";
 
     for (i = 0; i < NoOfOwnCmds; i++)
     {
@@ -240,8 +487,37 @@ int ownCmdHandler(char **parsed)
         calculator();
         return 1;
     case 6:
-        Useit();
+        // permutate();
         return 1;
+    case 7:
+        showTime();
+        return 1;
+    case 8:
+        piramide();
+        return 1;
+    case 9:
+        palindrome();
+        return 1;
+    case 10:
+        readWrite(parsed);
+        return 1;
+
+    case 11:
+        list();
+        return 1;
+    case 12:
+        copyFile(parsed);
+        return 1;
+    case 13:
+        move(parsed);
+        return 1;
+    case 14:
+        clearTerminal();
+        return 1;
+    case 15:
+        removeFile(parsed);
+        return 1;
+
     default:
         break;
     }
@@ -343,9 +619,26 @@ int main()
 
 void showLogo()
 {
-
-    
     printf("\n");
+    printf("  _________.__                         _____.__    .___  .___.__        \n");
+    printf(" /   _____/|  |__ _____ _______  _____/ ____\\__| __| _/__| _/|__| ____  \n");
+    printf(" \\_____  \\ |  |  \\__  \\_  __ \\/  _ \\   __\\|  |/ __ |/ __ | |  |/    \\ \n");
+    printf(" /        \\|   Y  \\/ __ \\|  | \\(  <_> )  |  |  / /_/ / /_/ | |  |   |  \n");
+    printf("/_______  /|___|  (____  /__|   \\____/|__|  |__\\____ \\____ | |__|___|  /\n");
+    printf("        \\/      \\/     \\/                           \\/    \\/         \\/ \n");
+    printf("  ________      ___.         .__       .__  .__                         \n");
+    printf(" /  _____/_____ \\_ |_________|__| ____ |  | |  | _____                  \n");
+    printf("/   \\  ___\\__  \\ | __ \\_  __ \\  |/ __ \\|  | |  | \\__  \\                \n");
+    printf("\\    \\_\\  \\/ __ \\| \\_\\ \\  | \\/  \\  ___/|  |_|  |__/ __ \\_              \n");
+    printf(" \\______  (____  /___  /__|  |__|\\___  >____/____(____  /              \n");
+    printf("        \\/     \\/    \\/              \\/               \\/               \n");
+    printf("   _____ ___.          .__   _____              .__                    \n");
+    printf("  /  _  \\_ |__   ____ |  |_/ ____\\____  _______|  |                   \n");
+    printf(" /  /_\\  \\| __ \\ /  _ \\|  |\\   __\\__  \\ \\___   /  |                   \n");
+    printf("/    |    \\ \\_\\ (  <_> )  |_|  |   / __ \\_/    /|  |__                \n");
+    printf("\\____|__  /___  /\\____/|____/__|  (____  /_____ \\____/                \n");
+    printf("        \\/    \\/                       \\/      \\/                     \n");
+    printf(".___                      __                                         \n");
     printf("|   | ____   ____  __ ___/  |_\n");
     printf("|   |/  _ \\ /    \\|  |  \\   __\\ \n");
     printf("|   (  <_> )   |  \\  |  /|  |   \n");
--- move/mv.c
+++ move/mv.c
@@ -9,6 +9,7 @@ int main(int argc, char *argv[])
     int dst; //destination file
     int n;   // tracking variable
     char buf[1];
+    printf(argv[1]);
     if (argc != 3)
     {
         printf("usage ./a.out sourceFileName destinationFileName\n");
